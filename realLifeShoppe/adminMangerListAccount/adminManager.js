24db9fccdf37b98343b37265afc6f1afec33c9ea
UIT.23730168 <Phannhathoa13@gmail.com>
2024-11-26 23:47:33 +0700

    new

--- realLifeShoppe/adminMangerListAccount/adminManager.js
+++ realLifeShoppe/adminMangerListAccount/adminManager.js
@@ -1,5 +1,6 @@
 
 let listAccount = getListAccount();
+let listChecked = [];
 showListAccount()
 function Voucher(code, discount) {
     this.code = code;
@@ -11,15 +12,86 @@ function showListAccount() {
 }
 function searchUsername(event) {
     let listAccount = getListAccount();
+
     const inputvalue = event.target.value;
     let searchUser = listAccount.filter((user) => user.username.includes(inputvalue));
-    listAccount.forEach((accounts) => {
-        document.getElementById(`username${accounts.username}`).remove();
-        document.getElementById(`checkBox${accounts.username}`).remove();
+
+    let checkBoxContainer = document.getElementById('checkBox')
+    let usernameContainer = document.getElementById('username');
+    let checkBoxChild = checkBoxContainer.children;
+    let usernameChild = usernameContainer.children;
+    checkBoxChild.forEach((index) => {
+        if (checkBoxChild[index].checked) {
+            listChecked.push(searchUser);
+        }
     })
-    if (inputvalue) {
-        createDOM(searchUser);
+}
+// function displayVoucherStatus(event) {
+//     let listAccount = getListAccount();
+//     const inputVoucherValue = event.target.value;
+//     let searchVoucherExist = listAccount
+//         .filter((account) => account.voucher
+//             .some((voucher) => voucher.code
+//                 .includes(inputVoucherValue)))
+//     removeCheckBoxAndUsernameDom(listAccount);
+//     if (inputVoucherValue) {
+//         createDOM(searchVoucherExist);
+//     }
+// }
+
+function Voucher(code, discount) {
+    this.code = code;
+    this.discount = discount;
+}
+function addVoucherToAccount() {
+    let listAccount = getListAccount();
+
+    const voucher = document.getElementById('voucher').value;
+    const discount = document.getElementById('discount').value;
+    const voucherList = new Voucher(voucher, discount);
+
+    let checkBoxContainer = document.getElementById('checkBox')
+    let usernameContainer = document.getElementById('username');
+    let checkBoxChild = checkBoxContainer.children;
+    let usernameChild = usernameContainer.children;
+
+    for (let index = 0; index < checkBoxChild.length; index++) {
+        if (checkBoxChild[index].checked) {
+            const username = usernameChild[index].textContent;
+            let account = getVoucherFromAccount(username);
+            if (account) {
+                account.voucher.push(voucherList);
+                listAccount = updatedListAccount(listAccount, account);
+            }
+        }
     }
+    saveAccountToLocalStorage(listAccount);
+}
+
+function removeVoucher() {
+    let listAccount = getListAccount();
+
+    let checkBoxContainer = document.getElementById('checkBox')
+    let usernameContainer = document.getElementById('username');
+    let checkBoxChild = checkBoxContainer.children;
+    let usernameChild = usernameContainer.children;
+
+    const voucher = document.getElementById('voucher').value;
+
+    for (let index = 0; index < checkBoxChild.length; index++) {
+        if (checkBoxChild[index].checked) {
+            let username = usernameChild[index].textContent;
+            let account = getVoucherFromAccount(username);
+            if (account) {
+                let voucherToRemove = getCodeVoucherFromAccount(account, voucher);
+                if (voucher == voucherToRemove.code) {
+                    account.voucher.splice(voucherToRemove.code, 1);
+                    listAccount = updatedListAccount(listAccount, account);
+                }
+            }
+        }
+    }
+    saveAccountToLocalStorage(listAccount);
 }
 function createDOM(listAccount) {
     listAccount.forEach((accounts) => {
@@ -35,38 +107,35 @@ function createDOM(listAccount) {
     });
 }
 
-function addVoucherToAccount() {
-    const voucherName = document.getElementById('voucher').value;
-    const discount = document.getElementById('discount').value;
-    const voucherList = new Voucher(voucherName, discount);
-    const checkBoxContainer = document.getElementById('checkBox');
-    const usernameContainer = document.getElementById('username');
-    const checkBox = checkBoxContainer.children;
-    const usernames = usernameContainer.children;
-    for (let index = 0; index < checkBox.length; index++) {
-        if (checkBox[index].checked) {
-           const usernameDom = usernames[index].textContent;
-           console.log(usernameDom);
-           
-        }
-        
-    }
+function removeCheckBoxAndUsernameDom(listAccount) {
+    listAccount.forEach((accounts) => {
+        document.getElementById(`username${accounts.username}`).remove();
+        document.getElementById(`checkBox${accounts.username}`).remove();
+    })
+}
+function updatedListAccount(listAccount, account) {
+    return listAccount.map((_accounts) =>
+        _accounts.username == account.username ? account : _accounts
+    );
 }
 
-function setAccountToLocalStorage(listAccount) {
-    return localStorage.setItem('accounts', JSON.stringify(listAccount));
+function saveAccountToLocalStorage(listAccount) {
+    return localStorage.setItem("accounts", JSON.stringify(listAccount));
 }
-function isCheckBoxChecked(usernameID) {
-    const username = document.getElementById(usernameID).textContent;
-    return username;
+
+function getCodeVoucherFromAccount(account, inputVoucher) {
+    return account.voucher.find((voucher) => voucher.code == inputVoucher);
 }
-function getVoucherFromAccount(usernameID) {
+
+function getVoucherFromAccount(usernames) {
     let listAccount = getListAccount();
-    return listAccount.find((user) => user.username == usernameID);
+    return listAccount.find((user) => user.username == usernames);
 }
-function isVoucherExistedOnAccount(voucherName) {
-    return listAccount.some((_voucher) => _voucher.voucher == voucherName);
-}
+
 function getListAccount() {
     return JSON.parse(localStorage.getItem('accounts'));
+}
+
+function windowAlert(string) {
+    window.alert(string);
 }
